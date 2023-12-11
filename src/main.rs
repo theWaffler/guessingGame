57cfda1598aea0d0572b7b28b986f6df355e89b8
theWaffler <gjudianto@gmail.com>
2023-12-10 17:19:25 -0800

    added rng function v1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io::{self, Write};
+use rand::Rng;
 
 fn main() {
     println!("Guess the fucking number!"); // adds new line
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     print!("Please input your guess: "); // doesnt add new line
 
     io::stdout().flush().expect("Failed to flush stdout"); // Flush the buffer
@@ -12,4 +14,11 @@ fn main() {
         .read_line(&mut guess) //mutable. default is immutable
         .expect("Failed to read line");
     println!("You Guess: {}", guess.trim());
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!!")
+
+    }
 }
